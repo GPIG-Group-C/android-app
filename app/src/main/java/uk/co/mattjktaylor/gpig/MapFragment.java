0dde3cf74c9de59714695606deed8ce4b9c2bcf0
Matthew Taylor <mjkt500@york.ac.uk>
2018-05-20 10:33:29 +0100

    Redesigned layout ready for incident list menu and restructured MapFragment

--- app/src/main/java/uk/co/mattjktaylor/gpig/MapFragment.java
+++ app/src/main/java/uk/co/mattjktaylor/gpig/MapFragment.java
@@ -1,21 +1,14 @@
 package uk.co.mattjktaylor.gpig;
 
-import android.content.DialogInterface;
 import android.content.res.Resources;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.EditText;
-import android.widget.Spinner;
-import android.widget.Switch;
-import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -28,16 +21,9 @@ import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polygon;
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-
-import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Locale;
 import java.util.UUID;
 
 public class MapFragment extends Fragment implements OnNotificationListener, OnMapReadyCallback, GoogleMap.OnPolygonClickListener, GoogleMap.OnInfoWindowClickListener, GoogleMap.OnMapLongClickListener {
@@ -53,7 +39,6 @@ public class MapFragment extends Fragment implements OnNotificationListener, OnM
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.map_fragment_layout, container, false);
-        setHasOptionsMenu(true);
 
         mMapView = (MapView) rootView.findViewById(R.id.mapView);
         mMapView.onCreate(savedInstanceState);
@@ -99,228 +84,17 @@ public class MapFragment extends Fragment implements OnNotificationListener, OnM
     }
 
     @Override
-    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        inflater.inflate(R.menu.menu_map, menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == R.id.action_add)
-            return true;
-
-        switch (item.getItemId()) {
-            case R.id.action_add_circle:
-                addCircle(new MapCircle("1", 37.75961, -122.4269, 2500, Calendar.getInstance().getTimeInMillis()));
-                return true;
-
-            case R.id.action_add_marker:
-                MapDescription desc = new MapDescription(50, "N/A", 0, null, null,
-                        null, "Test", Calendar.getInstance().getTimeInMillis());
-                addMarker(new MapMarker("0", 0, 37.72961, -122.4269,
-                        "Title 0", desc));
-                addMarker(new MapMarker("1", 1, 37.75961, -122.4269,
-                        "Title 1", desc));
-                addMarker(new MapMarker("2", 2, 37.76961, -122.4269,
-                        "Title 2", desc));
-                addMarker(new MapMarker("3", 3, 37.77961, -122.4269,
-                        "Title 3", desc));
-                addMarker(new MapMarker("4", 4, 37.78961, -122.4269,
-                        "Title 4", desc));
-                addMarker(new MapMarker("5", 5, 37.79961, -122.4269,
-                        "Title 5", desc));
-                addMarker(new MapMarker("6", 6, 37.74961, -122.4269,
-                        "Title 6", desc));
-                addMarker(new MapMarker("7", 7, 37.73961, -122.4269,
-                        "Title 7", desc));
-                return true;
-
-            case R.id.action_location:
-                centerMap();
-                return true;
-
-            case R.id.action_add_heatmap:
-                addHeatMap(new MapHeatMap(UUID.randomUUID().toString(), 37.74961, -122.4169, 40, 10.0, Calendar.getInstance().getTimeInMillis()));
-                addHeatMap(new MapHeatMap(UUID.randomUUID().toString(), 37.76961, -122.4369, 50, 5.0, Calendar.getInstance().getTimeInMillis()));
-                return true;
-
-            case R.id.action_add_polygon:
-                ArrayList<LatLng> coords = new ArrayList<>();
-                coords.add( new LatLng(37.74961, -122.4169));
-                coords.add( new LatLng(37.76961, -122.4369));
-                coords.add( new LatLng(37.73961, -122.4269));
-
-                MapDescription.Utility util = new MapDescription.Utility(true, false, false, true);
-                MapDescription.BuildingInfo bInfo = new MapDescription.BuildingInfo("Apartment Block", 1995);
-                MapDescription descr = new MapDescription(500, "Address", 1, util, bInfo, "First Responder","Testing", Calendar.getInstance().getTimeInMillis());
-                addPolygon(new MapPolygon("1", 1, 1, coords, descr));
-
-                coords.clear();
-                coords.add( new LatLng(37.71961, -122.4069));
-                coords.add( new LatLng(37.72961, -122.4069));
-                coords.add( new LatLng(37.72961, -122.4369));
-                coords.add( new LatLng(37.71961, -122.4369));
-                addPolygon(new MapPolygon("2", 1, 2, coords, descr));
-
-                coords.clear();
-                coords.add( new LatLng(37.75961, -122.4069));
-                coords.add( new LatLng(37.77961, -122.4069));
-                coords.add( new LatLng(37.77961, -122.4369));
-                coords.add( new LatLng(37.75961, -122.4369));
-                addPolygon(new MapPolygon("3", 1, 3, coords, descr));
-
-                return true;
-
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
-
-    @Override
     public void onMapLongClick(final LatLng latLng)
     {
-        // Get layout view for new incident:
-        LayoutInflater inflater = getActivity().getLayoutInflater();
-        final View dialogView = inflater.inflate(R.layout.dialog_incident, null);
-        // Setup spinner for incident types:
-        Spinner spinner = (Spinner) dialogView.findViewById(R.id.spinner_type);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),
-                R.array.incident_types, R.layout.spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
-        // Show dialog using created view:
-        AlertDialog.Builder alertSearch = new AlertDialog.Builder(getActivity());
-        alertSearch.setTitle("Add Incident At Location");
-        alertSearch.setView(dialogView)
-                // Add action buttons
-                .setPositiveButton("Submit", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int id)
-                    {
-                        EditText infoDescription = (EditText) dialogView.findViewById(R.id.edit_description);
-                        String info = infoDescription.getText().toString();
-
-                        Spinner typeSpinner = (Spinner) dialogView.findViewById(R.id.spinner_type);
-                        int type = typeSpinner.getSelectedItemPosition();
-
-                        // Add new marker using form data:
-                        MapDescription.Utility utility = new MapDescription.Utility();
-                        MapDescription description = new MapDescription(1, "", 1, utility, null,
-                                "First Responder", info, Calendar.getInstance().getTimeInMillis());
-
-                        MapMarker m = new MapMarker(UUID.randomUUID().toString(), type, latLng.latitude, latLng.longitude, "Incident", description);
-                        // Add to map:
-                        addMarker(m);
-                        // Send to server:
-                        ClientUsage.sendMarker(m);
-
-                    }
-                }).setNegativeButton("Cancel", null);
-        alertSearch.show();
+        IncidentDialog dialog = new IncidentDialog(getActivity(), this, latLng);
+        dialog.show();
     }
 
-    // TODO Needs refactoring:
     @Override
     public void onInfoWindowClick(Marker marker)
     {
-        marker.hideInfoWindow();
-
-        // TODO should probably make superclass:
-        // Find marker:
-        MapMarker mapMarker = null;
-        MapPolygon mapPolygon = null;
-        MapDescription mapDescription = null;
-        for(MapMarker m : MapFragment.markers)
-        {
-            if(m.getMarker().getId().equals(marker.getId()))
-            {
-                mapMarker = m;
-                break;
-            }
-        }
-        if(mapMarker == null)
-        {
-            for(MapPolygon p : MapFragment.polygons)
-            {
-                if(p.getMarker().getId().equals(marker.getId()))
-                {
-                    mapPolygon = p;
-                    break;
-                }
-            }
-
-            if(mapPolygon == null)
-                return;
-            else
-            {
-                mapDescription = mapPolygon.getDescription();
-            }
-        }
-        else
-        {
-            mapDescription = mapMarker.getDescription();
-        }
-
-        // Get layout view for new incident:
-        LayoutInflater inflater = getActivity().getLayoutInflater();
-        final View dialogView = inflater.inflate(R.layout.dialog_incident_update, null);
-
-        // Populate using existing data:
-        final Spinner spinner_sev = (Spinner) dialogView.findViewById(R.id.spinner_severity);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), R.layout.spinner_item, CustomInfoWindow.severities);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner_sev.setAdapter(adapter);
-        spinner_sev.setSelection(mapDescription.getStatus());
-
-        final EditText people = (EditText) dialogView.findViewById(R.id.text_people);
-        people.setText(String.format(Locale.ENGLISH, "%d", mapDescription.getNumPeople()));
-        final EditText info = (EditText) dialogView.findViewById(R.id.text_additional_info);
-        info.setText(mapDescription.getInfo());
-
-        final Switch switchGas = (Switch) dialogView.findViewById(R.id.utilties_gas_switch);
-        final Switch switchElec = (Switch) dialogView.findViewById(R.id.utilties_electricity_switch);
-        final Switch switchWater = (Switch) dialogView.findViewById(R.id.utilties_water_switch);
-        final Switch switchSewage = (Switch) dialogView.findViewById(R.id.utilties_sewage_switch);
-
-        if(mapDescription.getUtilities() != null)
-        {
-            dialogView.findViewById(R.id.utilities_container).setVisibility(View.VISIBLE);
-            switchGas.setChecked(mapDescription.getUtilities().isGas());
-            switchElec.setChecked(mapDescription.getUtilities().isElectricity());
-            switchWater.setChecked(mapDescription.getUtilities().isWater());
-            switchSewage.setChecked(mapDescription.getUtilities().isSewage());
-        }
-
-        // TODO sort out final duplication
-        final MapDescription mapDescription_ = mapDescription;
-        final MapPolygon mapPolygon_ = mapPolygon;
-        final MapMarker mapMarker_ = mapMarker;
-
-        // Show dialog using created view:
-        AlertDialog.Builder alertSearch = new AlertDialog.Builder(getActivity());
-        alertSearch.setTitle("Update Incident:");
-        alertSearch.setView(dialogView)
-                // Add action buttons
-                .setPositiveButton("Update", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int id)
-                    {
-                        mapDescription_.setStatus(spinner_sev.getSelectedItemPosition());
-                        mapDescription_.setInfo(info.getText().toString());
-                        mapDescription_.setDateAdded(Calendar.getInstance().getTimeInMillis());
-                        mapDescription_.setNumPeople(Integer.parseInt(people.getText().toString()));
-                        mapDescription_.setReportBy("First Responder");
-
-                        if(mapDescription_.getUtilities() != null)
-                            mapDescription_.setUtilities(new MapDescription.Utility(switchGas.isChecked(), switchElec.isChecked(), switchWater.isChecked(), switchSewage.isChecked()));
-
-                        if(mapMarker_ != null)
-                            ClientUsage.sendMarker(mapMarker_);
-
-                        else if(mapPolygon_ != null)
-                            ClientUsage.sendPolygon(mapPolygon_);
-                    }
-                }).setNegativeButton("Cancel", null);
-        alertSearch.show();
+        IncidentDialog dialog = new IncidentDialog(getActivity(), marker);
+        dialog.show();
     }
 
     @Override
@@ -335,15 +109,13 @@ public class MapFragment extends Fragment implements OnNotificationListener, OnM
                 break;
             }
         }
-
         if(mp != null)
         {
             mp.getMarker().showInfoWindow();
         }
-
     }
 
-    private void centerMap() {
+    public void centerMap() {
         LatLng location = new LatLng(37.75961, -122.4269);
         CameraPosition cameraPosition = new CameraPosition.Builder().target(location).zoom(12).build();
         googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
--- app/src/main/java/uk/co/mattjktaylor/gpig/MenuActivity.java
+++ app/src/main/java/uk/co/mattjktaylor/gpig/MenuActivity.java
@@ -1,54 +1,38 @@
 package uk.co.mattjktaylor.gpig;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AlertDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
-import android.support.design.widget.NavigationView;
 
-public class MenuActivity extends AppCompatActivity implements  NavigationView.OnNavigationItemSelectedListener{
+import com.google.android.gms.maps.model.LatLng;
 
-    private SettingsFragment settingsFragment;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.UUID;
+
+public class MenuActivity extends AppCompatActivity {
+
     private MapFragment mapFragment;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-        if (findViewById(R.id.nav_framelayout) != null)
+        if (mapFragment == null)
         {
-            if (savedInstanceState != null)
-                return;
-            else
-            {
-                settingsFragment = new SettingsFragment();
-                settingsFragment.setArguments(getIntent().getExtras());
-                mapFragment = new MapFragment();
-                mapFragment.setArguments(getIntent().getExtras());
-
-                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-                transaction.add(R.id.nav_framelayout, mapFragment).commit();
-            }
+            mapFragment = (MapFragment) getSupportFragmentManager().findFragmentById(R.id.map_fragment);
         }
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Live Map");
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
 
         ServerClient.init(this);
     }
@@ -64,42 +48,91 @@ public class MenuActivity extends AppCompatActivity implements  NavigationView.O
     }
 
     @Override
-    public boolean onNavigationItemSelected(MenuItem item)
+    public boolean onCreateOptionsMenu(Menu menu)
     {
-        final FragmentManager manager = getSupportFragmentManager();
-        final FragmentTransaction transaction = manager.beginTransaction();
-        transaction.addToBackStack(getSupportActionBar().getTitle().toString());
-
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-        if(id == R.id.nav_map)
-        {
-            getSupportActionBar().setTitle("Live Map");
-            transaction.replace(R.id.nav_framelayout, mapFragment);
-            transaction.commit();
-        }
-        else if(id == R.id.nav_settings)
-        {
-            getSupportActionBar().setTitle("Settings");
-            transaction.replace(R.id.nav_framelayout, settingsFragment);
-            transaction.commit();
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
+        getMenuInflater().inflate(R.menu.menu_map, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_add)
+            return true;
+
+        switch (item.getItemId()) {
+            case R.id.action_add_circle:
+                mapFragment.addCircle(new MapCircle("1", 37.75961, -122.4269, 2500, Calendar.getInstance().getTimeInMillis()));
+                return true;
+
+            case R.id.action_add_marker:
+                MapDescription desc = new MapDescription(50, "N/A", 0, null, null,
+                        null, "Test", Calendar.getInstance().getTimeInMillis());
+                mapFragment.addMarker(new MapMarker("0", 0, 37.72961, -122.4269,
+                        "Title 0", desc));
+                mapFragment.addMarker(new MapMarker("1", 1, 37.75961, -122.4269,
+                        "Title 1", desc));
+                mapFragment.addMarker(new MapMarker("2", 2, 37.76961, -122.4269,
+                        "Title 2", desc));
+                mapFragment.addMarker(new MapMarker("3", 3, 37.77961, -122.4269,
+                        "Title 3", desc));
+                mapFragment.addMarker(new MapMarker("4", 4, 37.78961, -122.4269,
+                        "Title 4", desc));
+                mapFragment.addMarker(new MapMarker("5", 5, 37.79961, -122.4269,
+                        "Title 5", desc));
+                mapFragment.addMarker(new MapMarker("6", 6, 37.74961, -122.4269,
+                        "Title 6", desc));
+                mapFragment.addMarker(new MapMarker("7", 7, 37.73961, -122.4269,
+                        "Title 7", desc));
+                return true;
+
+            case R.id.action_location:
+                mapFragment.centerMap();
+                return true;
+
+            case R.id.action_add_heatmap:
+                mapFragment.addHeatMap(new MapHeatMap(UUID.randomUUID().toString(), 37.74961, -122.4169, 40, 10.0, Calendar.getInstance().getTimeInMillis()));
+                mapFragment.addHeatMap(new MapHeatMap(UUID.randomUUID().toString(), 37.76961, -122.4369, 50, 5.0, Calendar.getInstance().getTimeInMillis()));
+                return true;
+
+            case R.id.action_add_polygon:
+                ArrayList<LatLng> coords = new ArrayList<>();
+                coords.add( new LatLng(37.74961, -122.4169));
+                coords.add( new LatLng(37.76961, -122.4369));
+                coords.add( new LatLng(37.73961, -122.4269));
+
+                MapDescription.Utility util = new MapDescription.Utility(true, false, false, true);
+                MapDescription.BuildingInfo bInfo = new MapDescription.BuildingInfo("Apartment Block", 1995);
+                MapDescription descr = new MapDescription(500, "Address", 1, util, bInfo, "First Responder","Testing", Calendar.getInstance().getTimeInMillis());
+                mapFragment.addPolygon(new MapPolygon("1", 1, 1, coords, descr));
+
+                coords.clear();
+                coords.add( new LatLng(37.71961, -122.4069));
+                coords.add( new LatLng(37.72961, -122.4069));
+                coords.add( new LatLng(37.72961, -122.4369));
+                coords.add( new LatLng(37.71961, -122.4369));
+                mapFragment.addPolygon(new MapPolygon("2", 1, 2, coords, descr));
+
+                coords.clear();
+                coords.add( new LatLng(37.75961, -122.4069));
+                coords.add( new LatLng(37.77961, -122.4069));
+                coords.add( new LatLng(37.77961, -122.4369));
+                coords.add( new LatLng(37.75961, -122.4369));
+                mapFragment.addPolygon(new MapPolygon("3", 1, 3, coords, descr));
+
+                return true;
+
+            case R.id.action_settings:
+                startActivity(new Intent(this, SettingsActivity.class));
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     @Override
     public void onBackPressed()
     {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START))
-        {
-            drawer.closeDrawer(GravityCompat.START);
-        }
-        else
-        {
             FragmentManager fm = getSupportFragmentManager();
             if (fm.getBackStackEntryCount() > 0)
             {
@@ -120,6 +153,5 @@ public class MenuActivity extends AppCompatActivity implements  NavigationView.O
                             }
                         }).create().show();
             }
-        }
     }
 }
